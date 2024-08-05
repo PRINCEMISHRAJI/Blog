e3e145fa565c8ead1c0a1a9f04615ce72811dc8f
Ankit <hellcyberworld@gmail.com>
2024-08-06 00:32:31 +0530

    added Login using React Hook Form

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -2,6 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
+import Button from './Button'
+import Input from './Input'
 
 
-export {Header,Footer, Logo, Container}
+export {Header,Footer, Logo, Container, Input, Button}
